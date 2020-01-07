30892afa4b2c41e4b88031a2e18ca1bf79bed585
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-01-07 21:27:00 +0800

    修改

--- src/Primary_algorithm/Chapter6/T4.java
+++ src/Primary_algorithm/Chapter6/T4.java
@@ -11,20 +11,15 @@ public class T4 {
             int[] dp=new int[len];
             dp[0]=nums[0];
             dp[1]=Math.max(nums[0],nums[1]);
-            int ans=0;
-            for(int i=2;i<nums.length;i++){
-                if(ans<dp[i-2]){
-                    ans=dp[i-2];
+            for(int i=2;i<len;i++){
+                if(dp[i-1]>=dp[i-2]+nums[i]){
+                    dp[i]=dp[i-1];
                 }
-                dp[i]=ans+nums[i];
-            }
-            int res=0;
-            for(int i=0;i<dp.length;i++){
-                if(res<dp[i]){
-                    res=dp[i];
+                else {
+                    dp[i]=dp[i-2]+nums[i];
                 }
             }
-            return res;
+            return dp[len];
         }
         return 0;
 
