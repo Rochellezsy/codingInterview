677ecde57c46e330ec7ca89ac934c85b2af58fd9
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-01-15 21:40:03 +0800

    1.15(1)

---
+++ src/Primary_algorithm/Chapter3/T6.java
@@ -0,0 +1,20 @@
+package Primary_algorithm.Chapter3;
+
+import java.util.List;
+
+public class T6 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
