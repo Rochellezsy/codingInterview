16c30c6ef037b9323da364151c50d8f5a6b31761
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-02-24 13:52:41 +0800

    学习基本数据结构2.24(2)

---
+++ src/Usual_algorithm/MyStack.java
@@ -0,0 +1,36 @@
+package Usual_algorithm;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+    Queue<Integer>queue;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue=new LinkedList<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        int count=queue.size();
+        queue.add(x);
+        while(count>=1){
+            queue.add(queue.poll());
+        }
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.poll();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return queue.peek();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty() ;
+    }
+}
