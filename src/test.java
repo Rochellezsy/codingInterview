906e6fa122322d74569b99ab6f8d07d88750501b
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-03-20 23:25:21 +0800

    3.20难搞，神啊请保佑我吧

--- src/test.java
+++ src/test.java
@@ -7,7 +7,6 @@ public class test {
 
     public test(List<String> melodies) {
         melodies.sort(String::compareTo);
-
         this.melodies = melodies;
         // 进行排序, 时间复杂度O(log(n))
 
@@ -21,15 +20,15 @@ public class test {
             for (int i = j; i < mel.length; i++) {
                 if (i == j) {
                     dp[j] = mel[j].length();
-    } else if (mel[j].charAt(mel[j].length() - 1) <= mel[i].charAt(0)) {
-        dp[j] = Math.max(dp[j], mel[j].length() + dp[i]);
-    }
-}
-        }
-                return dp[0];
+                } else if (mel[j].charAt(mel[j].length() - 1) <= mel[i].charAt(0)) {
+                    dp[j] = Math.max(dp[j], mel[j].length() + dp[i]);
                 }
+            }
+        }
+        return dp[0];
+    }
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
         List<String>melodies=new ArrayList<>();
         melodies.add("aaa");
         melodies.add("zzz");
