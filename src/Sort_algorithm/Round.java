979cac6cb31606bb45a31237f08737e80743ead5
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-04-22 00:25:27 +0800

    4.21 大莹加油

---
+++ src/Sort_algorithm/Round.java
@@ -0,0 +1,12 @@
+package Sort_algorithm;
+
+public class Round {
+
+    public static void main(String[] args) {
+        System.out.println(Math.round(-2.1));
+
+        double r=-2.1;
+        System.out.println((r > 0.0) ? (int)(r + 0.5) : (int)(r - 0.5));
+
+    }
+}
