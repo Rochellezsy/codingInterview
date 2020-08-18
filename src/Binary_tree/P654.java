4bd1fde1cf79f03bca289ca152021c80b27e4f2f
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-08-18 17:56:50 +0800

    大莹变厉害了，可以可以，加油

---
+++ src/Binary_tree/P654.java
@@ -0,0 +1,22 @@
+package Binary_tree;
+
+import java.util.Arrays;
+
+public class P654 {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if(nums.length==0)return null;
+        int index=0;
+        int ans = nums[0];
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>ans){
+                index = i;
+                ans = nums[i];
+            }
+        }
+        TreeNode root = new TreeNode(nums[index]);
+        root.left = constructMaximumBinaryTree(Arrays.copyOfRange(nums, 0, index));
+        root.right = constructMaximumBinaryTree(Arrays.copyOfRange(nums, index+1, nums.length));
+        return root;
+
+    }
+}
---
+++ src/Binary_tree/TreeNode.java
@@ -0,0 +1,11 @@
+package Binary_tree;
+
+public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int val;
+
+    public TreeNode(int val) {
+        this.val=val;
+    }
+}
