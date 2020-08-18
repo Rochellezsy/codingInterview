aaf73d5f37fb8d8ccdf5042ca9f70502876001fb
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-08-18 17:23:33 +0800

    大莹最棒了，棒棒

--- src/Heap/test.java
+++ src/Heap/test.java
@@ -1,34 +1,34 @@
 package Heap;
 
-public class MaxHeap {
-    int[]a;
+public class test {
+    int []a;
     int size;
 
-    public MaxHeap(int capacity){
-        this.a=new int[capacity+1];
+    public test(int capacity){
+        this.a = new int[capacity+1];
         this.size = 0;
         a[0] = Integer.MAX_VALUE;
     }
 
-    public boolean isfull(){
-        if(size == a.length-1){
-            System.out.println("heap is full");
+    public boolean isEmpty(){
+        if(size==0){
+            System.out.println("heap is Empty!");
             return true;
         }
         return false;
     }
 
-    public boolean isEmpty(){
-        if(size == 0){
-            System.out.println("heap is empty");
+    public boolean isFull(){
+        if(size == a.length-1){
+            System.out.println("heap is Full!");
             return true;
         }
         return false;
     }
 
     public void insert(int val){
-        if(isfull()){
-            System.out.println("is full");
+        if(isFull()){
+            System.out.println("insert failed!");
         }
         size++;
         a[size] = val;
@@ -37,7 +37,7 @@ public class MaxHeap {
 
     public int popMax(){
         if(isEmpty()){
-            System.out.println("is empty");
+            System.out.println("pop Max failed");
         }
         int temp = a[size];
         size--;
@@ -50,17 +50,17 @@ public class MaxHeap {
     public void swim(int k){
         while(k>1 && a[k]>a[k/2]){
             swap(a, k, k/2);
-            k = k/2;
+            k=k/2;
         }
     }
 
     public void sink(int k){
         while(2*k<=size){
-            int child = 2*k;
+            int child=2*k;
             if(child<size && a[child]<a[child+1]){
                 child++;
             }
-            if(a[k]<a[child]){
+            if(a[child]>a[k]){
                 swap(a, k, child);
                 k=child;
             }
@@ -71,9 +71,9 @@ public class MaxHeap {
     }
 
     public void swap(int[]a, int i, int j){
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+        int temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
     }
 
     public void print(){
@@ -84,15 +84,17 @@ public class MaxHeap {
     }
 
     public static void main(String[] args) {
-        MaxHeap test=new MaxHeap(6);
+        test t=new test(6);
         int[] nums={10,8,2,5,3,11};
         for (int num : nums) {
-            test.insert(num);
+            t.insert(num);
         }
-        test.print();
+        t.print();
         for(int i=0; i<3; i++){
-            System.out.println(test.popMax());
-            test.print();
+            System.out.println(t.popMax());
+            t.print();
         }
     }
+
+
 }
